c2de2a44c40f903eb837b9b231ce4326f1624b9d
Vishal <vshlvrm232@gmail.com>
2020-04-07 14:01:16 +0530

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Developer Portfolio
-
-This Repo contains my portfolio. React.js is used to develop the front end of the this web-app.
---
+++ precache-manifest.abba86f1cdc7e6c266fc40dfe0de21f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2451eab97d7593ef231d9d9a3bb69d10",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0799cb2153474d57af3c",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d6ec71f6acee5e9ee290",
+    "url": "/portfolio/static/css/main.229caf44.chunk.css"
+  },
+  {
+    "revision": "0799cb2153474d57af3c",
+    "url": "/portfolio/static/js/2.3551944c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.3551944c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ec71f6acee5e9ee290",
+    "url": "/portfolio/static/js/main.6b43e309.chunk.js"
+  },
+  {
+    "revision": "6f23035f47e5426de047",
+    "url": "/portfolio/static/js/runtime-main.7593b994.js"
+  },
+  {
+    "revision": "4efa888d8b09c89546a7503d18dd1934",
+    "url": "/portfolio/static/media/Background.4efa888d.jpg"
+  }
+]);
